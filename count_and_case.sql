90dd83fb61962b97d56ac1189b689ad63a3e88b3
kabinprajapati <85050829+kabinprajapati@users.noreply.github.com>
2023-06-10 21:29:48 +0900

    Update count_and_case.sql
    unwanted file

--- count_and_case.sql
+++ count_and_case.sql
@@ -1,15 +1,1 @@
--- create a table to count the number of customers broken down by store_id (in rows), and active status (in column)
 
-SELECT 
-    store_id,
-    COUNT(CASE
-        WHEN active = 1 THEN customer_id
-        ELSE NULL
-    END) AS active,
-    COUNT(CASE
-        WHEN active = 0 THEN customer_id
-        ELSE NULL
-    END) AS inactive
-FROM
-    customer
-GROUP BY store_id
