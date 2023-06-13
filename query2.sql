a02edcb74665ad44020c2a96450bfb9c8b7762b7
kabinprajapati <85050829+kabinprajapati@users.noreply.github.com>
2023-06-13 23:30:57 +0900

    Update and rename join.sql to query2.sql

--- query2.sql
+++ query2.sql
@@ -1,3 +1,9 @@
+ -- Query 2:
+
+-- From the doctors table, fetch the details of doctors who work in the same hospital but in different speciality.
+
+--Tables Structure:
+ 
  create database git
  use git
  drop table doctors;
