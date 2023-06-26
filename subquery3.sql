0ef632543fc9df8d6db105ab57726fb460117f86
kabinprajapati <85050829+kabinprajapati@users.noreply.github.com>
2023-06-26 19:30:08 +0900

    subquery3.sql added

---
+++ subquery3.sql
@@ -0,0 +1,9 @@
+-- Select local_name and lang_num from appropriate tables
+SELECT local_name, sub.lang_num
+FROM countries,
+    (SELECT code, COUNT(*) AS lang_num
+     FROM languages
+     GROUP BY code) AS sub
+-- Where codes match    
+WHERE countries.code = sub.code
+ORDER BY lang_num DESC;
